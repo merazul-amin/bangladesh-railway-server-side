381c1eec82312db961e29080ca3ab5074d03af43
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-01-04 11:06:49 +0600

    railway server initialized

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const hi = 100;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Railway Server is Running');
+})
+app.listen(port, () => {
+    console.log('Server is Running')
+})
+
+
+
