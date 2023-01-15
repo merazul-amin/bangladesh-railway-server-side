7613ea18d2704e53a7f6b71bbec5b4e1aa5866a2
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-01-15 12:40:47 +0600

    train search api added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //middleware
 app.use(cors());
@@ -11,6 +12,34 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Railway Server is Running');
 })
+
+
+
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.jnuj2ye.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run() {
+
+    app.get('/searchTrainsResults/search', async (req, res) => {
+        const trainQueryInfo = req.query;
+        console.log(trainQueryInfo);
+        console.log('hit')
+
+
+
+
+        const trainInfo = trainQueryInfo;
+        res.send({ ...trainInfo });
+    })
+
+
+}
+
+run().catch((err) => console.log(err));
+
 app.listen(port, () => {
     console.log('Server is Running')
 })
